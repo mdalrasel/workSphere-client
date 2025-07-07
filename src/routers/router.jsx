d382aa00992b7ee43452b088781b126b6d080926
mdalrasel <mdalrasel.dev@gmail.com>
2025-07-07 22:50:26 +0600

    Setup project with Vite, React Router, Tailwind CSS and Flowbite

---
+++ src/routers/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-5xl">Hello World</div>,
+  },
+]);
